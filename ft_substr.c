3e86140f6b211762f69042819edbd01116241a11
Paul Hautenauve <phautena@paul-f5Ar4s4.clusters.42paris.fr>
2024-08-01 11:39:45 +0200

    * Optimized ft_substr.

--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: phautena <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: phautena <phautena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 10:29:07 by phautena          #+#    #+#             */
-/*   Updated: 2024/06/04 17:06:26 by phautena         ###   ########.fr       */
+/*   Updated: 2024/08/01 11:18:31 by phautena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
 	size_t	i;
+	size_t	len_s;
 
+	len_s = ft_strlen(s);
 	if (s == NULL)
 		return (NULL);
-	if (start >= ft_strlen(s))
+	if (start >= len_s)
 		return (ft_strdup(""));
-	if (len > ft_strlen(s) - start)
-		len = ft_strlen(s) - start;
+	if (len > len_s - start)
+		len = len_s - start;
 	sub = malloc(sizeof(char) * (len + 1));
 	if (sub == NULL)
 		return (NULL);
